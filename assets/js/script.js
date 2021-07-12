d982fc4439e83e2e174a9372414767764b492ec9
Daniel Canales <xastrox@gmail.com>
2021-07-11 22:08:37 -0400

    Actualizacion de las tarjetas y comienzo del carrito de compras

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,55 @@
-const button = document.getElementById("button")
 const result = document.getElementById("result")
+const templateCard = document.getElementById("template-card").content
+const fragment = document.createDocumentFragment()
+let cart = {}
+
+document.addEventListener('DOMContentLoaded', () => catchTitle())
+
+result.addEventListener("click", event => {
+    addCart(event)
+})
 
 const catchTitle = async () => {
     const baseUrl = 'http://www.omdbapi.com/'
     try {
         const response = await fetch(`${baseUrl}/?apikey=f105dfc8&s="star wars"&type="movie"&page=1`)
         const data = await response.json()
-        return data
+        listMovies(data.Search)
     } catch (error) {
         console.error('ALGO SALIO MAL', error)
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => catchTitle().then(res => {
-    const list = res.Search
-    list.map(l => {result.innerHTML += `
-        <div class="card col-12 col-md-6 col-lg-4">
-            <img src="${l.Poster}" alt="Poster" style="width: 100%">
-            <div class="containerCard">
-                <h4><b>Titulo: ${l.Title}</b></h4>
-                <p>Valor $10.000</p>
-            </div>
-            <button>Detalles</button> <button>Comprar</button>
-        </div>
-    `})
-}))
+const listMovies = data => {
+    console.log(data)
+    data.map(movies => {
+        templateCard.querySelector("h5").textContent = movies.Title
+        templateCard.querySelector("span").textContent = 10000
+        templateCard.querySelector("img").setAttribute("src", movies.Poster)
+        templateCard.querySelector(".btn").dataset.id = movies.imdbID
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    result.appendChild(fragment)
+}
 
+const addCart = event => {
+    if(event.target.classList.contains("btn")) {
+        setCart(event.target.parentElement)
+    }
+    event.stopPropagation()
+}
 
+const setCart = obj => {
+    const movie = {
+        id : obj.querySelector(".btn").dataset.id,
+        title : obj.querySelector("h5").textContent,
+        price : obj.querySelector("span").textContent,
+        stock : 1
+    }
 
+    if(cart.hasOwnProperty(movie.id)) movie.stock = cart[movie.id].stock + 1
 
+    cart[movie.id] = {...movie}
+    console.log(cart)
+}
