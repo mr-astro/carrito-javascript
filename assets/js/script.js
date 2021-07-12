746ded4134f02c73cf0aa4c91a757ad3da70a1de
Daniel Canales <xastrox@gmail.com>
2021-07-12 01:28:31 -0400

    Pagina operativa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
 const result = document.getElementById("result")
+const cartTable = document.getElementById("cart-table")
+const cartFooter = document.getElementById("cart-footer")
 const templateCard = document.getElementById("template-card").content
+const templateCart = document.getElementById("template-cart").content
+const templateFooter = document.getElementById("template-footerCart").content
 const fragment = document.createDocumentFragment()
 let cart = {}
 
-document.addEventListener('DOMContentLoaded', () => catchTitle())
+document.addEventListener('DOMContentLoaded', () => {
+    catchTitle()
+    if(localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"))
+        saveCart()
+    }
+
+})
 
 result.addEventListener("click", event => {
     addCart(event)
+})
+
+cartTable.addEventListener("click", event => {
+    btnPlussLess(event)
 })
 
 const catchTitle = async () => {
@@ -21,7 +36,6 @@ const catchTitle = async () => {
 }
 
 const listMovies = data => {
-    console.log(data)
     data.map(movies => {
         templateCard.querySelector("h5").textContent = movies.Title
         templateCard.querySelector("span").textContent = 10000
@@ -34,9 +48,7 @@ const listMovies = data => {
 }
 
 const addCart = event => {
-    if(event.target.classList.contains("btn")) {
-        setCart(event.target.parentElement)
-    }
+    if(event.target.classList.contains("btn")) setCart(event.target.parentElement)
     event.stopPropagation()
 }
 
@@ -51,5 +63,69 @@ const setCart = obj => {
     if(cart.hasOwnProperty(movie.id)) movie.stock = cart[movie.id].stock + 1
 
     cart[movie.id] = {...movie}
-    console.log(cart)
+    saveCart()
+}
+
+const saveCart = () => {
+    cartTable.innerHTML = ""
+    Object.values(cart).forEach(product => {
+        templateCart.querySelector("th").textContent = product.id
+        templateCart.querySelectorAll("td")[0].textContent = product.title
+        templateCart.querySelectorAll("td")[1].textContent = product.stock
+        templateCart.querySelector(".btnPluss").dataset.id = product.id
+        templateCart.querySelector(".btnLess").dataset.id = product.id
+        templateCart.querySelector("span").textContent = product.stock * product.price
+
+        const clone = templateCart.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+
+    cartTable.appendChild(fragment)
+    totalFooter()
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+const totalFooter = () => {
+    cartFooter.innerHTML = " "
+    if(Object.keys(cart).length === 0) {
+        cartFooter.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>`
+        return
+    }
+
+    const sumStock = Object.values(cart).reduce((acum, {stock} ) => acum + stock, 0)
+    const sumPrice = Object.values(cart).reduce((acum, {stock, price} ) => acum + (stock * price),0) 
+    
+    templateFooter.querySelectorAll("td")[0].textContent = sumStock
+    templateFooter.querySelector("span").textContent = sumPrice
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    cartFooter.appendChild(fragment)
+
+    const btnDelete = document.getElementById("delete-cart")
+    btnDelete.addEventListener("click", () => {
+        cart = {}
+        saveCart()
+    })
+}
+
+const btnPlussLess = event => {
+    if(event.target.classList.contains("btnPluss")) {
+        const pluss = cart[event.target.dataset.id]
+        pluss.stock++
+        cart[event.target.dataset.id] = {...pluss}
+        saveCart()
+    }
+
+    if(event.target.classList.contains("btnLess")) {
+        const pluss = cart[event.target.dataset.id]
+        pluss.stock--
+        if(pluss.stock === 0) {
+            delete cart[event.target.dataset.id]
+        }
+        saveCart()
+    }
+
+    event.stopPropagation()
 }
